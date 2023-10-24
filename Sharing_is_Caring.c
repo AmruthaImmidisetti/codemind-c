10c7cfc430362d25ad14b45a05ddc896cf7b2cb0
AmruthaImmidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2023-10-24 17:44:44 +0530

    #include<stdio.h> int main() { 	int n,x; 	scanf("%d %d",&n,&x); 	if(n>=x && n>=(x*(x+1))/2) printf("YES"); 	else printf("NO");
    }

--- Sharing_is_Caring.c
+++ Sharing_is_Caring.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,x;
-    scanf("%d %d",&n,&x);
-    if(n>=(x*(x+1)/2)) printf("YES");
-    else printf("NO");
+	int n,x;
+	scanf("%d %d",&n,&x);
+	if(n>=x && n>=(x*(x+1))/2) printf("YES");
+	else printf("NO");
+	
 }
