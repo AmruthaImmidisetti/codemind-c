5582e50eff44a9d89393afa3b96baec2bc7baceb
AmruthaImmidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2023-12-13 22:59:53 +0530

    #include<stdio.h> int main() { int n,x; scanf("%d",&n); x=(n*n)/2.0; printf("%d",x); }

--- Magic_Pairs.c
+++ Magic_Pairs.c
@@ -1,19 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,even,odd;
-    scanf("%d",&n);
-    if(n%2==0)
-    {
-        even=(n/2);
-        odd=n-even;
-    }
-    else
-    {
-        odd=(n/2)+1;
-        even=n-odd;
-    }
-     int magicpair=(even*odd*2);
-     printf("%d",magicpair);
-    
+int n,x;
+scanf("%d",&n);
+x=(n*n)/2.0;
+printf("%d",x);
 }
