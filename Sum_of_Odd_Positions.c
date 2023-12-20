88a70d38205c18c6a8254ee47695f28e033e8789
AmruthaImmidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2023-12-20 19:56:48 +0530

    #include<stdio.h> int main() { 	int n,i,sum=0; 	scanf("%d",&n); 	int arr[n]; 	//reading array elements 	for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]); 	} 	//printing array elements 	for(i=0;i<n;i++) 	{ 	    if(i%2!=0) 	    { 		sum=sum+arr[i]; 	    } 	} 		printf("%d ",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,sum=0;
+	scanf("%d",&n);
+	int arr[n];
+	//reading array elements
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	//printing array elements
+	for(i=0;i<n;i++)
+	{
+	    if(i%2!=0)
+	    {
+		sum=sum+arr[i];
+	    }
+	}
+		printf("%d
+",sum);
+}
