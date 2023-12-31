5253e3565d65aa2de4320a7ca6322a8d8dc62ddb
AmruthaImmidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2023-12-31 14:40:52 +0530

    #include<stdio.h> int main() {     int n,i,s=0,avg,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {     scanf("%d",&arr[i]);
        s=s+arr[i];
        }
        avg=s/n;
        for(i=0;i<n;i++)
        {
            
          if(arr[i]>=avg)
          {
              c++;
          }
        }
        printf("%d",c);
    }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,s=0,avg,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&arr[i]);
+    
+    s=s+arr[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        
+      if(arr[i]>=avg)
+      {
+          c++;
+      }
+    }
+    printf("%d",c);
+}
