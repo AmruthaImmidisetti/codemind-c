ca10a0d9ccdcd05e4e1c1ad4710d454e6e0ec936
AmruthaImmidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2024-01-02 19:22:50 +0530

    #include<stdio.h> int main() {     int n,flag=0;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int a,b,max=0;     scanf("%d %d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             printf("%d ",arr[i]);         }
        }
        for(i=0;i<n;i++)
        {
            if(arr[i]%2==0)
            {
                printf("%d ",arr[i]);
            }
        
        }
       
    }

---
+++ Array_of_odd_and_even_elements.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,flag=0;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a,b,max=0;
+    scanf("%d %d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            printf("%d ",arr[i]);
+        }
+    
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("%d ",arr[i]);
+        }
+    
+    }
+   
+}
