bb118491b5dc112cb6e4ee89c3a9134747c05e4f
AmruthaImmidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2024-01-09 08:13:11 +0530

    #include<stdio.h> int main() {     long long int i,n,f=0,s=1,t;     scanf("%lld",&n);     for(i=0;i<n;i++)     {     	if(i<=1)    t=i;     	else 		{ 			t=f+s; 			f=s; 			s=t; 		} 		printf("%lld ",t); 	} }

--- fibonacci.c
+++ fibonacci.c
@@ -1,20 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,t,f=1,s=0;
-    scanf("%d",&n);
+    long long int i,n,f=0,s=1,t;
+    scanf("%lld",&n);
     for(i=0;i<n;i++)
     {
-        if(i<1) 
-        {
-            t=i;
-        }
-        else 
-        {
-            t=f+s;
-            f=s;
-            s=t;
-        }
-        printf("%d ",t);
-    }
+    	if(i<=1)    t=i;
+    	else
+		{
+			t=f+s;
+			f=s;
+			s=t;
+		}
+		printf("%lld ",t);
+	}
 }
