6ef4a47512991e2f03b62d18a04b6a9264935940
AmruthaImmidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2024-07-26 16:14:47 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {          scanf("%d",&arr[i]);     }     int sum=0;     for(int i=0;i<n;i++)     {         int q=arr[i];         int w=sqrt(q);         if(w*w==q)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

---
+++ Program_to_find_the_sum_of_perfect_square_elements_in_an_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+         scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        int q=arr[i];
+        int w=sqrt(q);
+        if(w*w==q)  
+        {
+            sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+}
